29dad3ba072377759a0ff804243effdb4e00b0ef
Rohit Kumar <rohit.kumar@girikon.com>
2024-08-13 20:39:51 +0530

    main

---
+++ main.js
@@ -0,0 +1,12 @@
+// scripts.js
+document.addEventListener('DOMContentLoaded', function () {
+    const navLinks = document.querySelectorAll('nav ul li a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(link.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
